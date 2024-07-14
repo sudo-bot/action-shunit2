e3287ae34c5ee6f772ba67d31a9fcf4e71696771
William Desportes <williamdes@wdes.fr>
2024-07-14 20:59:12 +0200

    Better debug

--- docker/tests/versions.sh
+++ docker/tests/versions.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-set -e
+set -eu
 
 cd $(dirname $0)/
 
 kcov --version
 SHUNIT_COLOR="none" ./test.sh | grep -F "Ran 1 test"
 kcov --collect-only /tmp/kcov ./test.sh
-ls -lah /tmp/kcov/index.html
+ls -lahR /tmp/kcov
