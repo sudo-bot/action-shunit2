1c61e862032083f006c514b3057ec69365a98e38
William Desportes <williamdes@wdes.fr>
2024-07-14 20:57:58 +0200

    Use --collect-only

--- docker/tests/versions.sh
+++ docker/tests/versions.sh
@@ -6,5 +6,5 @@ cd $(dirname $0)/
 
 kcov --version
 SHUNIT_COLOR="none" ./test.sh | grep -F "Ran 1 test"
-kcov /tmp/kcov ./test.sh
+kcov --collect-only /tmp/kcov ./test.sh
 ls -lah /tmp/kcov/index.html
