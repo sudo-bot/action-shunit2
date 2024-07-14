9b65bad615c3834b29d69f04de4a4dbb876181f5
William Desportes <williamdes@wdes.fr>
2024-07-14 21:02:55 +0200

    Use --dump-summary

--- docker/tests/versions.sh
+++ docker/tests/versions.sh
@@ -6,5 +6,5 @@ cd $(dirname $0)/
 
 kcov --version
 SHUNIT_COLOR="none" ./test.sh | grep -F "Ran 1 test"
-kcov --collect-only /tmp/kcov ./test.sh
+kcov --dump-summary /tmp/kcov ./test.sh
 ls -lahR /tmp/kcov
