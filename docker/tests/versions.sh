165e1d1a610c9423a256b9eaf5a06255e24eb750
William Desportes <williamdes@wdes.fr>
2024-07-14 21:05:05 +0200

    Better listing

--- docker/tests/versions.sh
+++ docker/tests/versions.sh
@@ -7,4 +7,4 @@ cd $(dirname $0)/
 kcov --version
 SHUNIT_COLOR="none" ./test.sh | grep -F "Ran 1 test"
 kcov --dump-summary /tmp/kcov ./test.sh
-ls -lahR /tmp/kcov
+ls -lahR /tmp/kcov/test.sh/*.{json,xml}
