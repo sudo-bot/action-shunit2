408c9b972c9a7b8ddf29ca2a74d5304fce676e2f
William Desportes <williamdes@wdes.fr>
2024-07-15 11:29:14 +0200

    Fixup coverage upload

--- docker/tests/versions.sh
+++ docker/tests/versions.sh
@@ -7,5 +7,4 @@ cd $(dirname $0)/
 kcov --version
 SHUNIT_COLOR="none" ./test.sh | grep -F "Ran 1 test"
 kcov --cobertura-only --dump-summary /tmp/kcov ./test.sh
-ls -lahR /tmp/kcov/
-find /tmp/kcov/test.sh/ -type f \( -name '*.json' -o -name '*.xml' \) -print
+find /tmp/kcov/ -type f \( -name '*.json' -o -name '*.xml' \) -print
