e1ff27edbeb35f6d6ac73e3c55da1dbff988599e
William Desportes <williamdes@wdes.fr>
2024-07-15 11:57:18 +0200

    More debug

--- docker/tests/versions.sh
+++ docker/tests/versions.sh
@@ -7,4 +7,3 @@ cd $(dirname $0)/
 kcov --version
 SHUNIT_COLOR="none" ./test.sh | grep -F "Ran 1 test"
 kcov --dump-summary ./coverage ./test.sh
-find ./coverage -type f \( -name '*.json' -o -name '*.xml' \) -print
