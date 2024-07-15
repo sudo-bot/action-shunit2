790e291f25f83567385a36e237eca45c26e42116
William Desportes <williamdes@wdes.fr>
2024-07-15 11:36:35 +0200

    More testing

--- docker/tests/versions.sh
+++ docker/tests/versions.sh
@@ -8,3 +8,5 @@ kcov --version
 SHUNIT_COLOR="none" ./test.sh | grep -F "Ran 1 test"
 kcov --cobertura-only --dump-summary ./coverage ./test.sh
 find ./coverage -type f \( -name '*.json' -o -name '*.xml' \) -print
+ls -lah ./coverage/cov.xml
+cat ./coverage/cov.xml
