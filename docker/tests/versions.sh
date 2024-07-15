2fba496034303c0b8a0f897de3d73b536f484d38
William Desportes <williamdes@wdes.fr>
2024-07-15 11:53:42 +0200

    Use directory mode

--- docker/tests/versions.sh
+++ docker/tests/versions.sh
@@ -6,7 +6,5 @@ cd $(dirname $0)/
 
 kcov --version
 SHUNIT_COLOR="none" ./test.sh | grep -F "Ran 1 test"
-kcov --cobertura-only --dump-summary ./coverage ./test.sh
+kcov --dump-summary ./coverage ./test.sh
 find ./coverage -type f \( -name '*.json' -o -name '*.xml' \) -print
-ls -lah ./coverage/cov.xml
-cat ./coverage/cov.xml
