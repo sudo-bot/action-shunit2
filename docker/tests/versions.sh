602baaf7777e7a4554ccfaac34c56483eb8bb317
William Desportes <williamdes@wdes.fr>
2024-07-15 11:27:41 +0200

    Upload the coverage

--- docker/tests/versions.sh
+++ docker/tests/versions.sh
@@ -6,5 +6,6 @@ cd $(dirname $0)/
 
 kcov --version
 SHUNIT_COLOR="none" ./test.sh | grep -F "Ran 1 test"
-kcov --dump-summary /tmp/kcov ./test.sh
+kcov --cobertura-only --dump-summary /tmp/kcov ./test.sh
+ls -lahR /tmp/kcov/
 find /tmp/kcov/test.sh/ -type f \( -name '*.json' -o -name '*.xml' \) -print
